229c314a2426230919b4374e79610b869233ccd0
Ankitkumar445 <169060244+Ankitkumar445@users.noreply.github.com>
2024-07-09 09:38:45 +0530

    Update Q4.java
    Line up

--- Icp Assing1/Q4.java
+++ Icp Assing1/Q4.java
@@ -6,8 +6,7 @@ public class Q4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print
-		("ankit\n");
+		System.out.print("ankit\n");
 		System.out.print("kumar\n");
 
 	}
